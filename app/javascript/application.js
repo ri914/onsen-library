3bcdc4fe666074a6741eec3e8f5afa75f13d9744
Apple <apple@Apples-MacBook-Air.local>
2024-09-19 18:43:36 +0900

    トップページ途中

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,40 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+$(function() {
+  $('.mainimg-tate').vegas({
+    slides: [
+      { src: 'images/E7DF5426-297B-4B63-91CB-F0ED40A9E910_1_105_c.jpeg' },
+      { src: 'images/FCEBF2BC-80A1-4449-A52A-F83CF9590C05_1_105_c.jpeg' },
+      { src: './images/tate_3.jpg' },
+    ],
+    transition: 'blur',
+    animation: 'kenburns',
+    delay: 6000,
+    animationDuration: 10000,
+    timer: false,
+  });
+
+  $('.mainimg-yoko').vegas({
+    slides: [
+      { src: './images/yoko_1.jpg' },
+      { src: './images/yoko_2.jpg' },
+      { src: './images/yoko_3.jpg' },
+    ],
+    transition: 'blur',
+    animation: 'kenburns',
+    delay: 6000,
+    animationDuration: 10000,
+    timer: false,
+  });
+
+  $('.mainimg-right').vegas({ // 右側のスライドショー設定
+    slides: [
+      { src: './images/right_1.jpg' }, // 1枚目の写真指定
+      { src: './images/right_2.jpg' }, // 2枚目の写真指定
+      { src: './images/right_3.jpg' }, // 3枚目の写真指定
+    ],
+    transition: 'blur',
+    animation: 'kenburns',
+    delay: 6000,
+    animationDuration: 10000,
+    timer: false,
+  });
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'home#top'
+  
   get "home/top"
   devise_for :users
-  root to: "home#top"
   post 'guest_login', to: 'sessions#guest_login'
+  get 'login', to: 'devise/sessions#new'
+  get 'signup', to: 'devise/registrations#new'
 end
