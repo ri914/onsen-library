e9ef53825831f8587fdd76b8b2cfdd9d2bcb60a2
Apple <apple@Apples-MacBook-Air.local>
2024-11-25 16:13:45 +0900

    [update]設定ページ完成

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -17,3 +17,16 @@ $('.login').on('submit', function(e) {
     }, 4000);
   }, 3000);
 });
+
+function updateImagePreview(input) {
+  if (input.files && input.files[0]) {
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      const avatarContainer = document.querySelector('.current-avatar-container img');
+      if (avatarContainer) {
+        avatarContainer.src = e.target.result;
+      }
+    }
+    reader.readAsDataURL(input.files[0]);
+  }
+}
